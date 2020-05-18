16529c0bd5d540df78c5fc69c212e032a00a0163
itoaki <itoaki.planet@gmail.com>
2020-05-18 12:05:35 +0900

    add project

---
+++ Assets/MyScripts/FollowPalyer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPalyer : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 ofset;
+    // Start is called before the first frame update
+    void Start()
+    {
+      ofset = this.transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        this.transform.position = player.transform.position + ofset;
+    }
+}
---
+++ Assets/MyScripts/GoalChecker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalChecker : MonoBehaviour
+{
+    public GameObject unityChan;
+    public AudioSource gameBGM;
+    public AudioSource goalBGM;
+    public GameObject retryButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+      retryButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+      other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+      unityChan.transform.LookAt(Camera.main.transform);
+      unityChan.GetComponent<Animator>().SetTrigger("Goal");
+      retryButton.SetActive(true);
+      gameBGM.Stop();
+      goalBGM.Play();
+    }
+
+    public void RetryStage() {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
